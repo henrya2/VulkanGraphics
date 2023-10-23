dc93b930584ef3cd4c292e82ff3801dd1a0992f7
Henry Read <henrya2@126.com>
2023-10-23 13:46:09 +0800

    adds a simple assets path initializer

---
+++ Source/Common/CustomAssetsPath.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <filesystem>
+#include <iostream>
+namespace fs = std::filesystem;
+
+struct AssetsPathInitializer
+{
+	AssetsPathInitializer(char* argv0)
+	{	
+		auto argv0Path = fs::path(argv0);
+		if (argv0Path.is_absolute())
+		{
+			fs::current_path(argv0Path.parent_path().parent_path());
+		}
+
+		std::cout << "Initialized current path: " << fs::current_path() << std::endl;
+	}
+};
+
+
